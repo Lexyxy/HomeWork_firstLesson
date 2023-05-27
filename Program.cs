8b28361e0202abb8d3acee78fffb944c85f3aecf
Lexy <alsik13@yandex.ru>
2023-05-27 23:38:02 +0300

    save6

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max>b)
+{
+    System.Console.WriteLine("max=" + a);
+    System.Console.WriteLine("min=" + b);
+}
+else 
+{
+    System.Console.WriteLine("max=" + b);
+    System.Console.WriteLine("min=" + a);
+}
